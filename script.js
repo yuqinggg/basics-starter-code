d95ef92be1ee28ef2d840bfc6b0244935c845493
yuqinggg <send2qing@gmail.com>
2021-10-19 07:44:39 +0800

    Edited code

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+// A user can enter the number of minutes past 1pm and the app will calculate the angle between the hour and minute hand.
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var angle1pm = 360 / 12;
+  var anglePerMinute = (360 / 60) * input;
+  var angle = anglePerMinute - angle1pm;
+  var myOutputValue = `The angle between the hour and minute hand is ${angle}`;
   return myOutputValue;
 };
